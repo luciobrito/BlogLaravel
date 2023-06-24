5e01ff5e3cb648c14d44d3e3b14fc0f55b02cfee
Lucio <kingdomlink69@gmail.com>
2023-06-23 22:52:49 -0300

    Main

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+</head>
+<body>
+    <h1>Primeiro contato com laravel!</h1>
+    <form action="/registrar" method="POST">
+        <input type="text" name="Nome" id="">
+        <input type="text" name="Email" id="">
+        <input type="password" placeholder="Senha" name="" id="">
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
